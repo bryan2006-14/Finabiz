16f766859bc463d7031bf62f7f89e1a0a84e1478
Fe73555930 <bdelgadollalli@gmail.com>
2025-09-29 13:54:43 -0500

    conex

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,16 +1,16 @@
-<?php
-try {
-    $host = getenv('DB_HOST');
-    $port = getenv('DB_PORT');
-    $dbname = getenv('DB_NAME');
-    $user = getenv('DB_USER');
-    $password = getenv('DB_PASSWORD');
+<?php 
+try { 
+    $host = "dpg-d3cp1eumcj7s73dpm8sg-a.oregon-postgres.render.com"; 
+    $port = "5432"; 
+    $dbname = "db_finanzas_fxs9"; 
+    $user = "db_finanzas_fxs9_user"; 
+    $password = "MzArnjJx2t87VeEF1Cr03C35Qv3M49CU"; 
 
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;";
-    $connection = new PDO($dsn, $user, $password, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
-} catch (PDOException $e) {
-    die("❌ Error de conexión: " . $e->getMessage());
-}
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;"; 
+    $connection = new PDO($dsn, $user, $password, [ 
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION 
+    ]); 
+} catch (PDOException $e) { 
+    die("❌ Error de conexión: " . $e->getMessage()); 
+} 
 ?>
