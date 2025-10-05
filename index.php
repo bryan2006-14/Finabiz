73c2d899f3a461a843fe18b7cef0d9b974be36e3
Fe73555930 <bdelgadollalli@gmail.com>
2025-10-04 23:54:24 -0500

    mejoras

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="icono-ic.png" sizes="96x96" type="image/x-icon">
-    <link rel="stylesheet" href="css/reset.css">
-    <title>Home</title>
-</head>
-<body>
 <?php
-// Redirige directamente al login antes de enviar cualquier HTML
 header("Location: login.php");
 exit();
 ?>
-
-</body>
-</html>
